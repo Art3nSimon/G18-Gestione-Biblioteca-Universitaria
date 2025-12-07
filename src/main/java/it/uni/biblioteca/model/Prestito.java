ee2fedded00a0073e285512a03e9af8f930d0620
Matteo <Matteo@MSI.mshome.net>
2025-12-07 10:57:02 +0100

    Prime implementazioni della classe Prestito: attributi, costruttore, metodi get

--- src/main/java/it/uni/biblioteca/model/Prestito.java
+++ src/main/java/it/uni/biblioteca/model/Prestito.java
@@ -5,10 +5,55 @@
  */
 package it.uni.biblioteca.model;
 
+import it.uni.biblioteca.model.Utente;
+import java.time.LocalDate;
+
 /**
  *
  * @author Matteo
  */
-public class Prestito {
+public class Prestito{
+    //Attributi Prestito
+    private String id;
+    private Utente utente;
+    private Libro libro;
+    private LocalDate dataPrestito;
+    private LocalDate dataRestituzionePrevista;
+    private LocalDate dataRestituzioneEffettiva;
+    
+    //Costruttore Prestito
+    public Prestito(String id, Utente utente, Libro libro, LocalDate dataRestituzione) {
+        this.id = id;
+        this.utente = utente;
+        this.libro = libro;
+        this.dataPrestito = LocalDate.now();
+        this.dataRestituzionePrevista = dataRestituzione;
+        this.dataRestituzioneEffettiva = null;
+    }
+    
+    //Metodi Get Prestito
+    public String getId() {
+        return id;
+    }
+    
+    public Utente getUtente() {
+        return utente;
+    }
+    
+    public Libro getLibro() {
+        return libro;
+    }
+    
+    public LocalDate getDataPrestito() {
+        return dataPrestito;
+    }
+    
+    public LocalDate getDataRestituzionePrevista() {
+        return dataRestituzionePrevista;
+    }
+    
+    public LocalDate getDataRestituzioneEffettiva() {
+        return dataRestituzioneEffettiva;
+    }
     
 }
